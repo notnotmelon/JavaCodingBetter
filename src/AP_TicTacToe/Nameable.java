566ee6100d1ca55d50abda4be27688f7f759c6f3
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-05-23 12:33:07 -0500

    Delete src directory

--- src/AP_TicTacToe/Nameable.java
+++
@@ -1,9 +0,0 @@
-package AP_TicTacToe;//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name - 
-
-public interface Nameable
-{
-	public void setName(String n);
-	public String getName();
-}
--- src/Directions.java
+++
@@ -1,8 +0,0 @@
-
-public final class Directions{
-    //3 = UpRight, 6 = DownRight, 12 = DownLeft, 9 = UpLeft
-    public static final int UP = 1, RIGHT = 2, DOWN = 4, LEFT = 8;
-
-    //making constructor private simulates static class
-    private Directions(){}
-}
