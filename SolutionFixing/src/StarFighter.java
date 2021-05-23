3e41cbbdd88d1711b9c8ef749087849d1f19b4e9
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-05-23 12:17:20 -0500

    Add files via upload

---
+++ SolutionFixing/src/StarFighter.java
@@ -0,0 +1,32 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import javax.swing.JFrame;
+import java.awt.Component;
+import java.awt.Graphics;
+
+public class StarFighter extends JFrame
+{
+	public static int WIDTH = 800;
+	public static int HEIGHT = 600;
+
+	public StarFighter()
+	{
+		super("STARFIGHTER");
+
+		setSize(WIDTH, HEIGHT);
+
+		OuterSpace theGame = new OuterSpace();
+		((Component)theGame).setFocusable(true);
+
+      getContentPane().add( theGame );
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main( String args[] )
+	{
+		StarFighter run = new StarFighter();
+	}
+}
